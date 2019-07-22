a9ebe61e6c08729e59fa490065feda52a06f2a96
Emmanuel Demey <demey.emmanuel@gmail.com>
2019-07-22 17:14:12 +0200

    feat: finish app

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,28 +1,80 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">My TODO List</h1>
+      <div class="field">
+        <div class="control">
+          <input
+            class="input"
+            type="text"
+            placeholder="Search"
+            v-model="newTask"
+            v-on:keyup.enter="submit"
+          />
+        </div>
+      </div>
+      <ul>
+        <Task v-for="task of tasks" :item="task" :key="task.id" @task-done="updateTask" />
+      </ul>
+      <footer class="footer" style="margin-top: 2em;">
+        <div class="content has-text-centered">
+          <p>
+            <strong>My TODO List</strong> by
+            <a href="https://twitter.com/emmanueldemey">Emmanuel DEMEY</a> and
+            <a href="https://twitter.com/AurelienLoyer">Aurelien LOYER</a>
+          </p>
+        </div>
+      </footer>
+    </div>
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Task from "./components/Task";
+import firebase from "firebase";
+
+const db = firebase
+  .initializeApp(...)
+  .firestore();
 
 export default {
-  name: 'app',
+  name: "app",
+
+  firestore: {
+    tasks: db.collection("todos")
+  },
+  data() {
+    return {
+      newTask: "",
+      tasks: [
+        { id: 0, label: "speak to Hack in woods", done: false },
+        { id: 1, label: "CFP DevDays", done: false }
+      ]
+    };
+  },
   components: {
-    HelloWorld
+    Task
+  },
+  methods: {
+    submit() {
+      this.tasks.push();
+      db.collection("todos").add({
+        label: this.newTask,
+        done: false
+      });
+      this.newTask = "";
+    },
+    updateTask(task) {
+      db.collection("todos")
+        .doc(task.id)
+        .set({
+          ...task,
+          done: !task.done
+        });
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ demo/src/components/Task.vue
@@ -0,0 +1,27 @@
+<template>
+  <li class="notification" :class="{ done: item.done}">
+    <button class="delete" @click="deleteTask"></button>
+    {{ item.label }}
+  </li>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    item: Object
+  },
+  methods: {
+    deleteTask() {
+      this.$emit("task-done", this.item);
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.done {
+  text-decoration: line-through;
+}
+</style>
